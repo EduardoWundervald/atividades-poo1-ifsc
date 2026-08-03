623b7a7ad7567dce5305c0a2bd44d5279d7fc7a1
Eduardo Wundervald <eduardowundervald1308@gmail.com>
2026-08-03 20:23:44 -0300

    Primeiros exercícios: declaração e atribuição de variáveise apresentação

---
+++ Apresentacao.java
@@ -0,0 +1,12 @@
+public class Apresentacao {
+    public static void main (String[] arg){
+        String nome = "Eduardo";
+        int idade = 20;
+        String curso = "Análise e Desenvolvimento de Sistemas";
+        String dataNasc = "20/10/05";
+        double nota = 8.7561;
+
+        System.out.printf("Nome: %s%n Idade: %d%n Curso: %s%n Data de Nascimento: %s%n Nota: %.2f%n", nome, idade, curso, dataNasc, nota);
+
+    }
+}
---
+++ Variavel.java
@@ -0,0 +1,6 @@
+public class Variavel {
+    public static void main (String[] arg){
+        int x = 10;
+        System.out.printf("O valor de x é: %d%n", x);
+    }
+}
